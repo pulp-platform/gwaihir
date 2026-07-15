60ad1cd3bf5d3dc971b4cbea85b9adfc71efa1f0
Chen Wu <chenwu@iis.ee.ethz.ch>
2026-07-15 17:16:37 +0200

    lint: blank space fix

--- hw/ucie_tile.sv
+++ hw/ucie_tile.sv
@@ -91,8 +91,8 @@ module ucie_tile
   // Chimney //
   /////////////
 
-  floo_gwaihir_noc_pkg::axi_narrow_in_req_t  axi_narrow_in_req, axi_narrow_in_req_canonical;
-  floo_gwaihir_noc_pkg::axi_wide_in_req_t    axi_wide_in_req, axi_wide_in_req_canonical;
+  floo_gwaihir_noc_pkg::axi_narrow_in_req_t axi_narrow_in_req, axi_narrow_in_req_can;
+  floo_gwaihir_noc_pkg::axi_wide_in_req_t axi_wide_in_req, axi_wide_in_req_can;
 
   floo_nw_chimney #(
     .AxiCfgN             (floo_gwaihir_noc_pkg::AxiCfgN),
@@ -149,28 +149,28 @@ module ucie_tile
 
   // Alias clearing
   always_comb begin
-    axi_narrow_in_req_canonical         = axi_narrow_in_req_i;
-    axi_narrow_in_req_canonical.aw.addr = axi_narrow_in_req_i.aw.addr & ~gwaihir_pkg::AliasClearMask;
-    axi_narrow_in_req_canonical.ar.addr = axi_narrow_in_req_i.ar.addr & ~gwaihir_pkg::AliasClearMask;
+    axi_narrow_in_req_can = axi_narrow_in_req_i;
+    axi_narrow_in_req_can.aw.addr = axi_narrow_in_req_i.aw.addr & ~gwaihir_pkg::AliasClearMask;
+    axi_narrow_in_req_can.ar.addr = axi_narrow_in_req_i.ar.addr & ~gwaihir_pkg::AliasClearMask;
 
-    axi_wide_in_req_canonical           = axi_wide_in_req_i;
-    axi_wide_in_req_canonical.aw.addr   = axi_wide_in_req_i.aw.addr & ~gwaihir_pkg::AliasClearMask;
-    axi_wide_in_req_canonical.ar.addr   = axi_wide_in_req_i.ar.addr & ~gwaihir_pkg::AliasClearMask;
+    axi_wide_in_req_can = axi_wide_in_req_i;
+    axi_wide_in_req_can.aw.addr = axi_wide_in_req_i.aw.addr & ~gwaihir_pkg::AliasClearMask;
+    axi_wide_in_req_can.ar.addr = axi_wide_in_req_i.ar.addr & ~gwaihir_pkg::AliasClearMask;
   end
 
   // Half-shift
   if (EnIngressHalfShift) begin : gen_ingress_half_shift
     always_comb begin
-      axi_narrow_in_req         = axi_narrow_in_req_canonical;
-      axi_narrow_in_req.aw.addr = ingress_half_shift(axi_narrow_in_req_canonical.aw.addr);
-      axi_narrow_in_req.ar.addr = ingress_half_shift(axi_narrow_in_req_canonical.ar.addr);
-      axi_wide_in_req           = axi_wide_in_req_canonical;
-      axi_wide_in_req.aw.addr   = ingress_half_shift(axi_wide_in_req_canonical.aw.addr);
-      axi_wide_in_req.ar.addr   = ingress_half_shift(axi_wide_in_req_canonical.ar.addr);
+      axi_narrow_in_req = axi_narrow_in_req_can;
+      axi_narrow_in_req.aw.addr = ingress_half_shift(axi_narrow_in_req_can.aw.addr);
+      axi_narrow_in_req.ar.addr = ingress_half_shift(axi_narrow_in_req_can.ar.addr);
+      axi_wide_in_req = axi_wide_in_req_can;
+      axi_wide_in_req.aw.addr = ingress_half_shift(axi_wide_in_req_can.aw.addr);
+      axi_wide_in_req.ar.addr = ingress_half_shift(axi_wide_in_req_can.ar.addr);
     end
   end else begin : gen_ingress_passthrough
-    assign axi_narrow_in_req = axi_narrow_in_req_canonical;
-    assign axi_wide_in_req   = axi_wide_in_req_canonical;
+    assign axi_narrow_in_req = axi_narrow_in_req_can;
+    assign axi_wide_in_req = axi_wide_in_req_can;
   end
 
 endmodule : ucie_tile
