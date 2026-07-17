b76fb8eba5146867c2a6a993d15fc58b8ea9c485
Chen Wu <chenwu@iis.ee.ethz.ch>
2026-07-17 17:40:54 +0200

    sw: modify ucie_loopback sw

--- sw/cheshire/tests/ucie_loopback.c
+++ sw/cheshire/tests/ucie_loopback.c
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+// Author: Chen Wu <chenwu@iis.ee.ethz.ch>
+
 #include <stdint.h>
 #include "params.h"
 #include "util.h"
@@ -12,13 +14,15 @@
 
 int main() {
 
-  // TODO: OR-ing ucie0's base hardcodes chiplet-select bit 25 = 1 (only chiplet0->chiplet1); 
-  //       a correct alias derives bit 25 from the destination chiplet.
-  volatile uint32_t *l2_chiplet1 = (volatile uint32_t *)((uintptr_t)&gwaihir_addrmap_64b.l2_spm | (uintptr_t)&gwaihir_addrmap_64b.ucie0);
-  (*l2_chiplet1) = TRANSFER_DATA;
+  // Write to chiplet1's L2 through the ucie0 alias window (routed via UCIe).
+  volatile uint32_t *alias_wr = (volatile uint32_t *)&gwaihir_addrmap_64b.ucie0.l2_spm[0];
+  // Read back through the canonical address (routed via the local NoC).
+  volatile uint32_t *local_rd = (volatile uint32_t *)&gwaihir_addrmap_64b.l2_spm[2];
+
+  (*alias_wr) = TRANSFER_DATA;
 
   fencei();
 
-  return (*l2_chiplet1 != TRANSFER_DATA);
+  return (*local_rd != TRANSFER_DATA);
 
 }
