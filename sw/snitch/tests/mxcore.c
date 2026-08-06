6a56eeddaaaa77886079668766c823a423b4a7ee
Jayanth Jonnalagadda <jjonnalagadd@sulperg.ee.ethz.ch>
2026-08-06 13:43:36 +0200

    MXCore Integration Updates
    [sw] Removed Redmule-related artifacts
    
    [Bender] MXCore bender dependency changed to HTTPS instead of SSH
    
    [Bender] Updated cluster tile dependency order in Bender.yml
    
    [sw] Updated MXCore's kernel to fix multi-cluster pointer corruption and contention issues
    
    [sw] Added verify.py for MXCore
    
    [CI] Added MXCore to CI

--- sw/snitch/tests/mxcore.c
+++ sw/snitch/tests/mxcore.c
@@ -38,10 +38,6 @@
 
 void mxcore_cfg (unsigned int vector_a_ptr, unsigned int vectors_b_ptr, unsigned int scale_a_ptr, unsigned int scale_b_ptr, unsigned int result_ptr, unsigned int result_scale_ptr, uint16_t m, uint16_t k, uint16_t n, uint32_t engine_ctrl_reg) {
   uint32_t nm = ((uint32_t)n << 16) | (uint32_t)m;
-  printf("(M, K, N): (%d, %d, %d)\n", m, k, n);
-  printf("(M, K, N): (%x, %x, %x)\n", m, k, n);
-  printf("NM Register Value: %x\n", nm);
-  printf("Control Engine Value: %x\n", engine_ctrl_reg);
   HWPE_WRITE(vector_a_ptr,      0x20);
   HWPE_WRITE(vectors_b_ptr,     0x24);
   HWPE_WRITE(scale_a_ptr,       0x28);
@@ -69,11 +65,10 @@ inline void snrt_hwpe_clr_mxip(uint32_t core_idx) {
     * (volatile uint32_t*)HWPE_MXIP_ADDR = (1 << core_idx);
 }
 
-volatile int mxstatus;
-
-void *local_a, *local_b, *local_scale_a, *local_scale_b, *local_result, *local_result_scale;
-
 int main() {
+    // if (snrt_cluster_idx() != 7) return 0;
+
+    void *local_a, *local_b, *local_scale_a, *local_scale_b, *local_result, *local_result_scale;
 
     uint32_t core_idx = snrt_cluster_core_idx();
 
@@ -97,13 +92,14 @@ int main() {
     uint32_t engine_ctrl = 0x00200678;
 
     // Allocate space and Copy Data into TCDM
+    local_a = snrt_l1_alloc_cluster_local(a_size, 4096);
+    local_b = snrt_l1_alloc_cluster_local(b_size, 4096);
+    local_scale_a = snrt_l1_alloc_cluster_local(scale_a_size, 4096);
+    local_scale_b = snrt_l1_alloc_cluster_local(scale_b_size, 4096);
+    local_result = snrt_l1_alloc_cluster_local(result_size, 4096);
+    local_result_scale = snrt_l1_alloc_cluster_local(result_scale_size, 4096);
+
     if (snrt_is_dm_core()) {
-        local_a = snrt_l1_alloc(a_size);
-        local_b = snrt_l1_alloc(b_size);
-        local_scale_a = snrt_l1_alloc(scale_a_size);
-        local_scale_b = snrt_l1_alloc(scale_b_size);
-        local_result = snrt_l1_alloc(result_size);
-        local_result_scale = snrt_l1_alloc(result_scale_size);
         snrt_dma_start_1d(local_a, vector_a, a_size);
         snrt_dma_start_1d(local_b, vectors_b, b_size);
         snrt_dma_start_1d(local_scale_a, scale_a, scale_a_size);
@@ -114,21 +110,16 @@ int main() {
 
     // Compute
     if (snrt_cluster_core_idx() == 0) {
-        printf("[Cycle: %u] Starting MXCore from Core %d\n", snrt_mcycle(), core_idx);
-
         mxcore_cg_enable();
 
         hwpe_soft_clear();
 
+        volatile int mxstatus;
         do {
             mxstatus = hwpe_acquire_job();
         } while (mxstatus < 0);
 
-        printf("[Cycle: %u] MXCore Acquired from Core %d\n", snrt_mcycle(), core_idx);
-
         mxcore_cfg((unsigned int) local_a, (unsigned int) local_b, (unsigned int) local_scale_a, (unsigned int) local_scale_b, (unsigned int) local_result, (unsigned int) local_result_scale, M, K, N, engine_ctrl);
-
-        printf("[Cycle: %u] MXCore Configured from Core %d\n", snrt_mcycle(), core_idx);
 
         hwpe_trigger_job();
 
@@ -136,53 +127,13 @@ int main() {
         snrt_interrupt_enable(IRQ_M_ACC);
         while ((hwpe_status = hwpe_get_status()) != 0) snrt_wfi();
 
-        printf("[Cycle: %u] MXCore Interrupt from Core %d\n", snrt_mcycle(), core_idx);
-
         snrt_hwpe_clr_mxip(core_idx);
+
         snrt_interrupt_disable(IRQ_M_ACC);
 
         mxcore_cg_disable();
-
-        printf("[Cycle: %u] MXCore Clear Interrupt from Core %d\n", snrt_mcycle(), core_idx);
     }
     snrt_cluster_hw_barrier();
-
-    // Compare DUT and Golden Results
-    if (snrt_cluster_core_idx() == 0) {
-        int errors = 0;
-        int elements = (QUANTIZE_OUTPUT == 1) ? (sizeof(golden_result_mx) / sizeof(uint8_t)) : (sizeof(golden_result) / sizeof(float));  // They will be equal anyway 
-        printf("[Cycle: %u] Starting Comparison of DUT and Golden Results\n", snrt_mcycle());
-        for (int i = 0; i < 32; i++) {
-            if (QUANTIZE_OUTPUT == 1) {
-                uint8_t golden = golden_result_mx[i];
-                uint8_t dut    = ((uint8_t *)local_result)[i];
-                if (dut != golden) {
-                    if (errors < 10) {
-                        printf("Mismatch at Index %d: Expected 0x%02x, Got 0x%02x\n", i, golden, dut);
-                    }
-                    errors++;
-                }
-            } else {
-                float golden = golden_result[i];
-                float dut = ((float *)local_result)[i];
-                float diff = golden - dut;
-                if (diff < 0) diff = -diff;
-                if (diff > 1e-5f) {
-                    if (errors < 10) {
-                        uint32_t gbits = *((uint32_t*)&golden);
-                        uint32_t dbits = *((uint32_t*)&dut);
-                        printf("Mismatch at Index %d: Expected 0x%08x, Got 0x%08x, Difference = %e\n", i, gbits, dbits, diff);
-                    }
-                    errors++;
-                }
-            }
-        }
-        if (errors == 0) {
-            printf("[Cycle: %u] Result Comparison Complete: Passed with No Errors\n", snrt_mcycle());
-        } else {
-            printf("[Cycle: %u]: Result Comparison Complete: Failed with %d Errors\n", snrt_mcycle(), errors);
-        }
-    }
 
     // Copy data out of TCDM
     if (snrt_is_dm_core()) {
