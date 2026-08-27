a0f10a2de0bd47ee5054be8643ddecc7dba7ecf3
Lionnus Kesting <github@lionn.us>
2026-08-27 16:12:41 +0200

    :arrow_up: htile: Bump Surya and add license to Surya headers

--- sw/snitch/tests/data/surya_mx/task0_mx_scale.h
+++ sw/snitch/tests/data/surya_mx/task0_mx_scale.h
@@ -1,3 +1,7 @@
+// Copyright 2026 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef TASK0_MX_SCALE_H
 #define TASK0_MX_SCALE_H
 
