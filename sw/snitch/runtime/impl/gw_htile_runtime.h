76be088f2d4ff1d3812af50377ad73f2deb89c67
Lionnus Kesting <github@lionn.us>
2026-08-31 16:20:39 +0200

    :bug: Remove reduction h-tile

--- sw/snitch/runtime/impl/gw_htile_runtime.h
+++ sw/snitch/runtime/impl/gw_htile_runtime.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "gw_noc_cfg.h"
 #include "gw_raw_addrmap_32b.h"
 #include "snitch_cluster_cfg.h"
 
@@ -13,7 +14,9 @@
 #undef SNRT_CLUSTER_NUM
 #undef SNRT_TCDM_START_ADDR
 
-#define SNRT_BASE_HARTID 145
+#define SNRT_BASE_HARTID \
+  (CFG_CLUSTER_BASE_HARTID + \
+   GW_CLUSTER_PER_ROW * GW_CLUSTER_PER_COL * CFG_CLUSTER_NR_CORES)
 #define SNRT_CLUSTER_NUM 1
 #define SNRT_TCDM_START_ADDR GW_HTILE_TCDM_BASE_ADDR
 
