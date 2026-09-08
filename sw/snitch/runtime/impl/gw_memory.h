da055cfebe5a1dc035cc01a4b56361735416574c
Lionnus Kesting <github@lionn.us>
2026-09-09 01:16:09 +0200

    :boot: htile: Run the H tile on the shared Snitch runtime

--- sw/snitch/runtime/impl/gw_memory.h
+++ sw/snitch/runtime/impl/gw_memory.h
@@ -5,6 +5,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef GW_HTILE_BASE_ADDR
+// The H tile takes the cluster slot after the cluster array.
+#define GW_HTILE_CLUSTER_IDX SNRT_CLUSTER_NUM
+#endif
+
 // TODO(colluca): add alias to addrmap so this can be properly implemented
 // Must return a pointer to the snitch_cluster_t struct
 // of the cluster alias.
@@ -15,18 +20,14 @@ inline volatile snitch_cluster_t* snrt_cluster_alias() {
 // Must return a pointer to the snitch_cluster_t struct
 // of the cluster selected by cluster_idx.
 inline volatile snitch_cluster_t* snrt_cluster(int cluster_idx) {
-#ifdef GW_HTILE_RUNTIME
-    (void)cluster_idx;
-    return (volatile snitch_cluster_t *)(uintptr_t)GW_HTILE_BASE_ADDR;
-#else
 #ifdef GW_HTILE_BASE_ADDR
-    if (cluster_idx == SNRT_CLUSTER_NUM) {
-        return (volatile snitch_cluster_t *)(uintptr_t)GW_HTILE_BASE_ADDR;
+    if (cluster_idx == GW_HTILE_CLUSTER_IDX) {
+        return &(gwaihir_addrmap_32b.htile);
     }
 #endif
-    return &(gwaihir_addrmap_32b.cluster[cluster_idx]);
-#endif
+    return (volatile snitch_cluster_t*)&(gwaihir_addrmap_32b.cluster[cluster_idx]);
 }
+
 // Must return a pointer to the snitch_cluster_t struct
 // of the cluster invoking the function.
 inline volatile snitch_cluster_t* snrt_cluster() {
