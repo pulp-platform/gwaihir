215ee02123fc67cf98c77408725f3895613bdf24
Gabriele Lodi <gabriele.lodi93@gmail.com>
2026-09-10 12:51:51 +0200

    hw: remove ucie interrupts, fix negative offset in ucie tile

--- hw/ucie_tile.sv
+++ hw/ucie_tile.sv
@@ -258,8 +258,8 @@ module ucie_tile
   } addr_rule_t;
 
   // Sam Idx offset between UCIe base and AxiCfg
-  localparam int AxiSerialCfgIdxOffset = int'(Ucie0AxiSerialCfgSamIdx) - int'(Ucie0SamIdx);
-  localparam int TileCfgIdxOffset = int'(Ucie0TileCfgSamIdx) - int'(Ucie0SamIdx);
+  localparam int AxiSerialCfgIdxOffset = int'(Ucie0SamIdx) - int'(Ucie0AxiSerialCfgSamIdx);
+  localparam int TileCfgIdxOffset = int'(Ucie0SamIdx) - int'(Ucie0TileCfgSamIdx);
 
   ucie_rule_t [TileCfgIdxOffset:0] tile_addrmap;
   // 3 rules: 2 for the AXI-Lite config (Tile Cfg + Slink Cfg), 1 for the join path.
