b962fe4e2074a826d527cef69540e86e8035f70d
Daniel Keller <daniel.kellermartinez@csem.ch>
2026-09-23 12:09:15 +0200

    sw: Name the snitch dep tests to build instead of inheriting them all

--- sw/sw.mk
+++ sw/sw.mk
@@ -55,7 +55,13 @@ SN_APPS += $(GW_SNITCH_SW_DIR)/apps/power_benchmarks
 # Spatz kernels
 SN_APPS += $(GW_SNITCH_SW_DIR)/apps/spatz-fmatmul
 
-SN_TESTS = $(wildcard $(GW_SNITCH_SW_DIR)/tests/*.c)
+SN_BUILD_TESTS = OFF
+
+SN_TESTS  = $(wildcard $(GW_SNITCH_SW_DIR)/tests/*.c)
+# The dep tests gwaihir runs; the rest assume the snitch address map
+SN_TESTS += $(SN_ROOT)/sw/tests/src/simple.c
+SN_TESTS += $(SN_ROOT)/sw/tests/src/non_null_exitcode.c
+SN_TESTS += $(SN_ROOT)/sw/tests/src/dma_transpose.c
 
 $(GW_GEN_SW_DIR)/gw_noc_cfg.h: $(SN_RUNTIME_SRCDIR)/gw_noc_cfg.h.tpl $(FLOO_CFG)
 	$(FLOO_GEN) template -c $(FLOO_CFG) $(FLOO_PARAMS) -o $(GW_GEN_SW_DIR) --no-format $<
